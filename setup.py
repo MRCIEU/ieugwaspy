6afa9a91a72765b40168a47c9bae4aec1672158d
t0mrg <tom.gaunt@bristol.ac.uk>
2020-04-18 15:08:34 +0100

    Added documentation

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
     name="ieugwaspy",
-    version="0.1",
+    version="0.1.1",
     author="Tom Gaunt",
     author_email="tom@biocompute.org.uk",
     description="Python interface to IEU GWAS database API",
@@ -18,4 +18,5 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.7",
+    install_requires=["requests"],
 )
