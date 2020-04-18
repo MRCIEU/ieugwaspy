40f781f31e60632157fe43e057b799bbe37404e5
t0mrg <tom.gaunt@bristol.ac.uk>
2020-04-18 12:33:13 +0100

    Added tests

---
+++ tests/test_api.py
@@ -0,0 +1,15 @@
+#!/usr/local/bin/python3
+import ieugwaspy
+
+def test_apistatus():
+    data = ieugwaspy.api_status()
+    assert(data['API version'])
+
+def test_variantstoid():
+    data = ieugwaspy.variants_to_rsid(['10:44865737'])
+    assert(data[0] == 'rs7777')
+    assert(len(data) == 1)
+
+def test_studylookup():
+    data = ieugwaspy.gwasinfo(['ieu-a-1239'])
+    assert(data[0]['trait'] == 'Years of schooling')
