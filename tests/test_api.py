d90eaefb1124fce5e33e79d2986f656dbbd7c86a
t0mrg <tom.gaunt@bristol.ac.uk>
2020-05-14 11:54:41 +0000

    Added different tests and vscode devcontainer config

--- tests/test_api.py
+++ tests/test_api.py
@@ -18,3 +18,30 @@ def test_studylookup():
     assert data[0]["trait"] == "Years of schooling"
     alldata = ieugwaspy.gwasinfo()
     assert len(alldata) > 10000
+
+
+def test_gwasinfo():
+    data = ieugwaspy.gwasinfo(['ieu-a-1239','ebi-a-GCST006867'])
+    assert len(data) == 2
+    assert len(data[0]) > 0
+    assert data[0]["pmid"] == 30038396
+    assert data[1]["pmid"] == 30054458
+
+
+def test_phewas():
+    data = ieugwaspy.phewas(["rs53576"])
+    assert len(data) > 0
+    assert len(data[0]) > 0
+    assert 0.0 <= data[0]["p"] <= 1.0
+
+
+def test_tophits():
+    data = ieugwaspy.tophits(["ukb-b-12014"])
+    assert len(data) > 0
+    assert 0.0 <= data[0]["p"] <= 1.0
+
+
+def test_associations():
+    data = ieugwaspy.associations(variantlist=["rs4363"],id=["ukb-b-12014","met-a-717"],access_token="NULL")
+    assert len(data) > 0
+    assert 0.0 <= data[0]["p"] <= 1.0
